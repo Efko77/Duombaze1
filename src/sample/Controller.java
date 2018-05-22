6d0eeda2410cb93e218bdcdcc2560a6cb8504661
Efko77 <github@.com>
2018-05-22 21:40:07 +0300

    nauji duomenys

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,13 @@
 package sample;
 
+import javafx.beans.property.Property;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.sql.Connection;
@@ -13,12 +19,38 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private TableView<Student>studentsTableView;
+
+    @FXML
+    private TableColumn<Student, Integer>idTableColum;
+
+    @FXML
+    private TableColumn<Student, String> nameTableColum;
+    @FXML
+    private TableColumn<Student, String> surnameTableColum;
+    @FXML
+    private TableColumn<Student, String> phoneTableColum;
+    @FXML
+    private TableColumn<Student, String> emailTableColum;
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) { //Suveikia pries uploadinant
         Connection connection = MyDbUtils.createConnection();
         if (connection != null) {
 
             List<Student> students = getStudents(connection);
+
+            idTableColum.setCellValueFactory(new PropertyValueFactory<Student, Integer>("id"));
+            nameTableColum.setCellValueFactory(new PropertyValueFactory<Student, String>("name"));
+            surnameTableColum.setCellValueFactory(new PropertyValueFactory<Student, String>("surname"));
+            phoneTableColum.setCellValueFactory(new PropertyValueFactory<Student, String>("phone"));
+            emailTableColum.setCellValueFactory(new PropertyValueFactory<Student, String>("email"));
+
+            studentsTableView.setItems(FXCollections.observableList(students));
 
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setContentText("Prisijungeme" + students.size());
